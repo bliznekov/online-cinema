b64a630c15dab9dbe83b569774951c0476a3aca3
bliznekov <r.bliznekov@gmail.com>
2022-07-26 23:14:21 +0300

    feat: implemented getNewTokens

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { hash, genSalt, compare } from 'bcryptjs'
 import { UserModel } from 'src/user/user.model'
 import { AuthDto } from './dto/auth.dto'
 import { JwtService } from '@nestjs/jwt'
+import { RefreshTokenDto } from './dto/refreshToken.dto'
 
 @Injectable()
 export class AuthService {
@@ -45,6 +46,23 @@ export class AuthService {
 
 		return {
 			user: this.returnUserFields(newUser),
+			...tokens,
+		}
+	}
+
+	async getNewTokens({ refreshToken }: RefreshTokenDto) {
+		if (!refreshToken) throw new UnauthorizedException('Please sign in!')
+
+		const result = await this.jwtService.verifyAsync(refreshToken)
+
+		if (!result) throw new UnauthorizedException('Invalid token or expired!')
+
+		const user = await this.UserModel.findById(result._id)
+
+		const tokens = await this.issueTokenPair(String(user._id))
+
+		return {
+			user: this.returnUserFields(user),
 			...tokens,
 		}
 	}
