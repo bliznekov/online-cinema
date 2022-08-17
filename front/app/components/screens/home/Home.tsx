e639d3bf01cb9f84e562944c730edad55da3167e
bliznekov <r.bliznekov@gmail.com>
2022-08-17 13:33:10 +0300

    feat: implement heading

--- front/app/components/screens/home/Home.tsx
+++ front/app/components/screens/home/Home.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 
 import Layout from '@/components/layout/Layout'
+import Heading from '@/components/ui/heading/Heading'
 
 import { Meta } from '@/utils/meta/Meta'
 
@@ -12,7 +13,10 @@ const Home: FC<IHome> = () => {
 			title="Watch movies online"
 			description="Watch MovieApp movies and TV shows online or stream right to your browser"
 		>
-			<h1>Home page</h1>
+			<Heading
+				title="Watch movies online"
+				className="text-gray-300 mb-8 text-xl"
+			/>
 		</Meta>
 	)
 }
