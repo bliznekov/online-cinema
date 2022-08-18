9a0fe996ebed359f31460fc4c71f566e0ac32ff0
bliznekov <r.bliznekov@gmail.com>
2022-08-18 13:00:41 +0300

    feat: implement favorites movie on Sidebar

---
+++ front/app/components/layout/Sidebar/MoviesContainer/FavoriteMovieList/FavoriteMovieList.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const FavoriteMovieList: FC = () => {
+	return <div></div>
+}
+
+export default FavoriteMovieList
---
+++ front/app/components/layout/Sidebar/MoviesContainer/MovieItem.tsx
@@ -0,0 +1,48 @@
+import { getGenreUrl, getMovieUrl } from 'config/url.config'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import MaterialIcon from '@/components/ui/MaterialIcon'
+
+import { getGenresListEach } from '@/utils/movie/getGenresList'
+
+import styles from './MovieList.module.scss'
+import { IWidgetMovie } from './movie.types'
+
+const MovieItem: FC<{ movie: IWidgetMovie }> = ({ movie }) => {
+	return (
+		<div className={styles.item}>
+			<Link href={getMovieUrl(movie.slug)}>
+				<a>
+					<Image
+						alt={movie.title}
+						width={65}
+						height={97}
+						src={movie.poster}
+						draggable={false}
+						priority
+					/>
+				</a>
+			</Link>
+			<div className={styles.info}>
+				<div>
+					<div className={styles.title}>{movie.title}</div>
+					<div className={styles.genres}>
+						{movie.genres.map(({ slug, name, _id }, idx) => (
+							<Link key={_id} href={getGenreUrl(slug)}>
+								<a>{getGenresListEach(idx, movie.genres.length, name)}</a>
+							</Link>
+						))}
+					</div>
+				</div>
+				<div className={styles.rating}>
+					<MaterialIcon name="MdStarRate" />
+					<span>{movie.rating.toFixed(1)}</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default MovieItem
