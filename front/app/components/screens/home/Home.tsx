f5d5b241d9f7e4c64c37a393764a3235bb45cff7
bliznekov <r.bliznekov@gmail.com>
2022-08-18 14:33:08 +0300

    feat: implement reduxtoolkit, toast & HeadProvider

--- front/app/components/screens/home/Home.tsx
+++ front/app/components/screens/home/Home.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 
-import Layout from '@/components/layout/Layout'
 import Heading from '@/components/ui/heading/Heading'
 
 import { Meta } from '@/utils/meta/Meta'
--- front/app/providers/MainProvider.tsx
+++ front/app/providers/MainProvider.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Provider } from 'react-redux'
 
 import Layout from '@/components/layout/Layout'
+
+import { store } from '@/store/store'
+
+import HeadProvider from './HeadProvider/HeadProvider'
+import ReduxToastr from './ReduxToastr'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -15,9 +21,14 @@ type T = { children: React.ReactNode }
 
 const MainProvider: FC<T> = ({ children }) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Layout>{children}</Layout>
-		</QueryClientProvider>
+		<HeadProvider>
+			<Provider store={store}>
+				<QueryClientProvider client={queryClient}>
+					<ReduxToastr />
+					<Layout>{children}</Layout>
+				</QueryClientProvider>
+			</Provider>
+		</HeadProvider>
 	)
 }
 
