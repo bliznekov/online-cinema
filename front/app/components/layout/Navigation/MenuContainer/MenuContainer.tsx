eaebc7a2a5d21363e21bbbab56cff54768f72615
bliznekov <r.bliznekov@gmail.com>
2022-08-23 23:20:20 +0300

    feat: implement auth-redux, user actions&services

--- front/app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
+++ front/app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
@@ -1,14 +1,18 @@
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 
 import Menu from './Menu'
-import GenreMenu from './genres/GenreMenu'
 import { firstMenu, userMenu } from './menu.data'
+
+const DynamicGenreMenu = dynamic(() => import('./genres/GenreMenu'), {
+	ssr: false,
+})
 
 const MenuContainer: FC = () => {
 	return (
 		<div>
 			<Menu menu={firstMenu} />
-			<GenreMenu />
+			<DynamicGenreMenu />
 			<Menu menu={userMenu} />
 		</div>
 	)
--- front/app/components/screens/auth/Auth.tsx
+++ front/app/components/screens/auth/Auth.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import Button from '@/components/ui/form-elements/Button'
 import Heading from '@/components/ui/heading/Heading'
 
+import { useActions } from '@/hooks/useActions'
 import { useAuth } from '@/hooks/useAuth'
 
 import { Meta } from '@/utils/meta/Meta'
@@ -29,8 +30,7 @@ const Auth: FC = () => {
 		mode: 'onChange',
 	})
 
-	const login = (data: any) => {}
-	const register = (data: any) => {}
+	const { login, register } = useActions()
 
 	const onSubmit: SubmitHandler<IAuthInput> = (data) => {
 		if (type === 'login') login(data)
