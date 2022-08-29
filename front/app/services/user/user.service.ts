22c1c386ba0051f3c423ad5bed6a4327bed9e1c1
bliznekov <r.bliznekov@gmail.com>
2022-08-29 22:33:07 +0300

    feat: implement template table, usersList

---
+++ front/app/services/user/user.service.ts
@@ -0,0 +1,20 @@
+import axios from 'api/interceptors'
+import { getUsersUrl } from 'config/api.config'
+
+import { IUser } from '@/shared/types/user.types'
+
+export const UserService = {
+	async getUsers(searchTerm?: string) {
+		return axios.get<IUser[]>(getUsersUrl(''), {
+			params: searchTerm
+				? {
+						searchTerm,
+				  }
+				: {},
+		})
+	},
+
+	async deleteUser(_id: string) {
+		return axios.delete<string>(getUsersUrl(`/${_id}`))
+	},
+}
